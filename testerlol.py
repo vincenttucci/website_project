0ac031ae6ead57654bc1457965d85ff05c0965a6
vincenttucci <110308520+vincenttucci@users.noreply.github.com>
2023-02-07 17:42:02 +1100

    Update testerlol.py

--- testerlol.py
+++ testerlol.py
@@ -3,7 +3,6 @@ import openai
 import os
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="My Webpage", page_icon = "lol", layout = "wide")
 
@@ -27,9 +26,6 @@ load_dotenv()
 openai.api_key = os.getenv("api_key")
 
 #WEBSITE DESIGNER
-#ASSETS
-computeranalystguy = loadnem("https://assets7.lottiefiles.com/packages/lf20_i2eyukor.json")
-#HEADER
 st.subheader("Introducing <Insert Name Here>")
 st.title("Alpha Version 0.1.03")
 
@@ -59,8 +55,7 @@ with st.container():
         st.text("the button. Wait about a minute and a document should generate.")
         st.text("Once the document has generated, a download button will appear that you can click.")
         st.text("Some precaution messages may appear but just accept them, it just downloads a docx.")
-    with right_column:
-        st_lottie(computeranalystguy, height = 300, key = "coding", loop=False)
+   
 
 
 
